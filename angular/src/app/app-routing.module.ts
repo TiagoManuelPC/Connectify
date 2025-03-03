8df7ceaa3b15afd9cc2bbfc1d68280d210468424
Tiago Manuel <tiagomanuelpereiracampos@hotmail.com>
2025-03-03 16:31:27 +0000

    update css and routing

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MembersComponent } from './members/members.component';
+import { authGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   // { path: 'services', component: ServicesComponent },
   // { path: 'blog', component: BlogComponent },
-  // { path: 'contact', component: ContactsComponent },
-  // { path: '', component: HomeComponent},
+  { path: 'members', component: MembersComponent, canActivate: [authGuard] },
+  { path: 'register', component: RegisterComponent},
   { path: '', redirectTo: 'login', pathMatch: 'full' }, // Default route
-  { path: '**', redirectTo: '/' } // Wildcard route for a 404 page
+  { path: '**', redirectTo: 'login' }
 ];
 
 @NgModule({
