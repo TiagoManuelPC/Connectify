97e16366163bb12fa8c1b3a190f24972afa76536
Tiago Manuel <tiagomanuelpereiracampos@hotmail.com>
2025-03-03 16:32:32 +0000

    remove redundant images

---
+++ angular/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:44337/api/'
+};
+
+// "launchUrl": "http://localhost:4200/",
+
+//dotnet ef migrations add IinitialCreate -o Data/Migrations
+//dotnet ef database update
