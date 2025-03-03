29f1697bdce4b1a3d185fb76601eba81255f0348
Tiago Manuel <tiagomanuelpereiracampos@hotmail.com>
2025-03-03 16:36:58 +0000

    Update .gitignore to exclude .vs directory

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,17 +1,27 @@
+using Connectify.Data;
+using Connectify.Interfaces;
+using Connectify.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAngularApp",
-        builder =>
-        {
-            builder.WithOrigins("http://localhost:4200")
-                   .AllowAnyHeader()
-                   .AllowAnyMethod();
-        });
+   options.AddPolicy("AllowAngularApp",
+       builder =>
+       {
+           builder.WithOrigins("http://localhost:4200", "https://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+       });
 });
+
+builder.Services.AddDbContext<DataContext>(options =>
+   options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddControllers();
+builder.Services.AddScoped<ITokenService, TokenService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,8 +31,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+   app.UseSwagger();
+   app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -35,3 +45,66 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+// using Connectify.Data;
+// using Connectify.Entities;
+// using Microsoft.AspNetCore.Identity;
+
+// var builder = WebApplication.CreateBuilder(args);
+
+// // add services to the container
+
+// //builder.Services.AddApplicationServices(builder.Configuration);
+// builder.Services.AddControllers();
+// builder.Services.AddCors();
+// //builder.Services.AddIdentityServices(builder.Configuration);
+// builder.Services.AddSignalR();
+
+// // Configure the HTTP request pipeline
+// builder.Services.AddSwaggerGen();
+// var app = builder.Build();
+
+// //app.UseMiddleware<ExceptionMiddleware>();
+
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+// app.UseHttpsRedirection();
+
+// app.UseCors(x => x.AllowAnyHeader()
+//     .AllowAnyMethod()
+//     .AllowCredentials()
+//     .WithOrigins("https://localhost:4200"));
+
+// app.UseAuthentication();
+// app.UseAuthorization();
+
+// app.UseDefaultFiles();
+// app.UseStaticFiles();
+
+// app.MapControllers();
+// //app.MapHub<PresenceHub>("hubs/presence");
+// //app.MapHub<MessageHub>("hubs/message");
+// //app.MapFallbackToController("Index", "Fallback");
+
+// AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+// using var scope = app.Services.CreateScope();
+// var services = scope.ServiceProvider;
+// try
+// {
+//     var context = services.GetRequiredService<DataContext>();
+//     var userManager = services.GetRequiredService<UserManager<AppUser>>();
+//     //var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
+//     //await context.Database.MigrateAsync();
+//     //await Seed.SeedUsers(userManager, roleManager);
+// }
+// catch (Exception ex)
+// {
+//     var logger = services.GetRequiredService<ILogger<Program>>();
+//     logger.LogError(ex, "An error occurred during migration");
+// }
+
+// await app.RunAsync();
