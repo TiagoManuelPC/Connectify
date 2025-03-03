cb26a19d8b7cc30b11d3d3ef91e787e7ca3b464c
Tiago Manuel <tiagomanuelpereiracampos@hotmail.com>
2025-03-03 16:31:55 +0000

    update app component

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    standalone: false,
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular';
+
+    constructor(public accountService: AccountService,
+        private router: Router)
+    { }
+
+    logout() {
+        this.accountService.logout();
+        this.router.navigateByUrl('/')
+    }
 }
